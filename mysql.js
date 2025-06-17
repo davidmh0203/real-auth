042ad23e4d21a968c125f9f02a2cf23549a47bb6
davidmh0203 <100127450+davidmh0203@users.noreply.github.com>
2025-06-17 16:43:01 +0900

    Update mysql.js

--- mysql.js
+++ mysql.js
@@ -1,11 +1,10 @@
-
+require("dotenv").config();
 const mysql = require("mysql2");
-
 const conn = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "12341234",
-  database: "minhyeong",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 conn.connect((err) => {
